d2ea24f3c93e1437f79639182b0521ef517caeb9
JWooChan <jwc6584@naver.com>
2025-03-11 12:52:40 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,30 @@
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int jull = 2 * n - 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n - i; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for (int k = 2 * i - 1; k > 0; k--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = n - 1; i > 0; i--)
+	{
+		for (int j = n - i; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for (int k = 2 * i - 1; k > 0; k--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
