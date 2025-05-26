44fd3669401c9a8848e6bbf80edc1f446c505d24
JWooChan <jwc6584@naver.com>
2025-05-26 13:10:46 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.c
@@ -0,0 +1,36 @@
+int main()
+{
+    int A, B, C;
+    char a[5], b[5], c[5];
+
+    while (1)
+    {
+        scanf("%d", &A);
+        if (A >= 1 && A <= 1000)
+            break;
+    }
+
+    while (1)
+    {
+        scanf("%d", &B);
+        if (B >= 1 && B <= 1000)
+            break;
+    }
+
+    while (1)
+    {
+        scanf("%d", &C);
+        if (C >= 1 && C <= 1000)
+            break;
+    }
+
+    sprintf(a, "%d", A);
+    sprintf(b, "%d", B);
+    sprintf(c, "%d", C);
+
+    printf("%d\n", A + B - C);
+    strcat(a, b);
+    printf("%d\n", atoi(a) - atoi(c));
+
+    return 0;
+}
